594a13ec76b29baf4b8e97923fbf5660c62e54f2
AriadnaRamirez <ariadnamts98@gmail.com>
2025-09-12 16:25:06 -0600

    Configuracion inicial

---
+++ PruebaTecnicaCRUD/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="flex items-center justify-center h-screen bg-gray-100">
+			<h1 className="text-6xl font-bold text-red-500">
+				¡Tailwind funciona! 🎉
+			</h1>
+		</div>
+	);
+}
+
+export default App;
