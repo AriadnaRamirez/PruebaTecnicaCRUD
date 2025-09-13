9054fde35bdc638e7fdcfd9660b60f8078f9428b
AriadnaRamirez <ariadnamts98@gmail.com>
2025-09-12 20:28:22 -0600

    CRUD con Zustand y avance en get all products

--- PruebaTecnicaCRUD/src/App.tsx
+++ PruebaTecnicaCRUD/src/App.tsx
@@ -1,11 +1,41 @@
-function App() {
-	return (
-		<div className="flex items-center justify-center h-screen bg-gray-100">
-			<h1 className="text-6xl font-bold text-red-500">
-				¡Tailwind funciona! 🎉
-			</h1>
-		</div>
-	);
+import { useEffect } from "react";
+import { useProductsStore } from "./store/useProductsStore";
+
+function ProductList() {
+  const { products, fetchProducts, deleteProduct } = useProductsStore();
+
+  // Cargar productos al montar el componente
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Lista de Productos </h1>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <li
+            key={product.id}
+            className="border p-4 rounded shadow hover:shadow-lg transition"
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-32 h-32 object-contain mx-auto"
+            />
+            <h2 className="font-semibold mt-2">{product.title}</h2>
+            <p className="text-gray-600">${product.price}</p>
+            <button
+              onClick={() => deleteProduct(product.id)}
+              className="mt-3 bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+            >
+              Eliminar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App;
+export default ProductList;
---
+++ PruebaTecnicaCRUD/src/store/useProductsStore.ts
@@ -0,0 +1,99 @@
+import { create } from "zustand";
+
+//interface
+export interface Product {
+	id: number;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+}
+
+//interface de los estados
+interface ProductState {
+	products: Product[];
+	fetchProducts: () => Promise<void>;
+	addProduct: (product: Omit<Product, "id">) => Promise<void>;
+	deleteProduct: (id: number) => Promise<void>;
+	editProduct: (product: Product) => Promise<void>;
+}
+
+//creacion de store
+export const useProductsStore = create<ProductState>((set) => ({
+	products: [],
+
+	// Obtiene 10 productos
+	fetchProducts: async () => {
+		try {
+			const res = await fetch(
+				"https://fakestoreapi.com/products?limit=10"
+			);
+			if (!res.ok) throw new Error("Error al obtener productos");
+			const data = await res.json();
+			set({ products: data });
+		} catch (error) {
+			console.error("fetchProducts error:", error);
+		}
+	},
+
+	// Editar un producto
+	editProduct: async (product: Product) => {
+		try {
+			const res = await fetch(
+				`https://fakestoreapi.com/products/${product.id}`,
+				{
+					method: "PUT",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(product),
+				}
+			);
+			if (!res.ok) throw new Error("Error al editar producto");
+			const editedProduct = await res.json();
+			set((state) => ({
+				products: state.products.map((p) =>
+					p.id === editedProduct.id ? editedProduct : p
+				),
+			}));
+		} catch (error) {
+			console.error("editProduct error:", error);
+		}
+	},
+
+	// Crear nuevo producto
+	addProduct: async (product: Omit<Product, "id">) => {
+		try {
+			const res = await fetch("https://fakestoreapi.com/products", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(product),
+			});
+			if (!res.ok) throw new Error("Error al crear producto");
+			const newProduct = await res.json();
+
+			// Aqui actualizo el array de productos para verlo reflejado en el front
+			set((state) => ({ products: [...state.products, newProduct] }));
+		} catch (error) {
+			console.error("addProduct error:", error);
+		}
+	},
+
+	// Eliminar un producto
+	deleteProduct: async (id: number) => {
+		try {
+			const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
+				method: "DELETE",
+			});
+			if (!res.ok) throw new Error("Error al eliminar producto");
+
+			// Aqui filtro el array de productos para verlo reflejado en el front
+			set((state) => ({
+				products: state.products.filter((product) => product.id !== id),
+			}));
+		} catch (error) {
+			console.error("deleteProduct error:", error);
+		}
+	},
+}));
